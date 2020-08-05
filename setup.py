5e8cd6ccc65d5d0d12d3c1cd7b54e16ba111d7ba
Oliver Green <green2go@gmail.com>
2020-08-05 19:32:20 +0200

    ⬆️ 1.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='Grand Travels',
     description='An app to track our travels via our motorhomes GPS enabled router.',
-    version='1.2',
+    version='1.3',
     long_description=__doc__,
     packages=find_packages(),
     include_package_data=True,
